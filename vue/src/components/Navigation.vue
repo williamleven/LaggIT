9b939334f202acf248fcf5880a21109889d4c2d1
Weyenriah <weyenriah@gmail.com>
2019-10-13 11:45:48 +0200

    navigation scaffolding

--- vue/src/components/Navigation.vue
+++ vue/src/components/Navigation.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="navigation">
-
+        <nav>
+            <router-link to="Home">Hem</router-link>
+            <router-link to="Events">Evenemang</router-link>
+            <router-link to="Member">Bli Medlem!</router-link>
+            <router-link to="About">Om LaggIT</router-link>
+        </nav>
     </div>
 </template>
 
